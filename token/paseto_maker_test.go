ea75d42d05468240500bf3a515a491dfe1b91b9c
lamdt <lam.dt26042000@gmail.com>
2023-11-29 16:53:59 +0700

    feat: implement paseto token

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,49 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/lamdangtung/golang-sample-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomUsername()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+	require.NotZero(t, payload.ID)
+	require.WithinDuration(t, payload.ExpiredAt, expiredAt, time.Minute)
+	require.WithinDuration(t, payload.IssuedAt, issuedAt, time.Minute)
+	require.Equal(t, payload.Username, username)
+
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomUsername()
+	duration := time.Minute
+	token, err := maker.CreateToken(username, -duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
